328fee60e5a41cdf92ea3f94f4fc0039d99ff30c
deeprathod97 <deeprathod9797@gmail.com>
2024-04-21 22:49:42 +0530

    javascrit Operators and Expressions

---
+++ script.js
@@ -0,0 +1,15 @@
+//  77;
+// "Deep Bhai"
+// false
+console.log("Operators in Js")
+
+let a = 45;
+let b = 4
+console.log("a + b =",a+b)
+console.log("a - b =",a-b)
+console.log("a / b =",a/b)
+console.log("a ** b =",a**b)
+console.log("a % b =",a%b)
+console.log("a ++ =",a++)
+console.log("a = ",a)
+console.log("a -- =",a--)
